2d30f56b731b6248924f6136820e0406b4316d56
Aurumpasha <aurumpasha@gmail.com>
2025-04-27 19:33:48 +0800

    item_create_2

--- tests/KMS_item_create.spec.ts
+++ tests/KMS_item_create.spec.ts
@@ -3,6 +3,7 @@ import { CREDENTIALS_CM } from '../config/credentials';
 import { MAIN_ENVIRONMENT } from '../config/environment_KMS';
 
 test('Item create', async ({ page }) => {
+  //Login
   await page.goto(MAIN_ENVIRONMENT.environment);
   await page.getByText('Username', { exact: true }).click();
   await page.getByRole('textbox', { name: 'Username*' }).fill(CREDENTIALS_CM.username);
@@ -12,18 +13,21 @@ test('Item create', async ({ page }) => {
   await page.getByTitle('Content Manager').click();
   await page.getByRole('listbox').getByRole('option', { name: 'Content Manager' }).click();
   await page.locator('#kms-login-to-layout-button').click();
+  //Check susscessfull login
+  await expect(page.getByRole('link', { name: 'Go to the home page' })).toBeVisible();
+  await expect(page.getByRole('button', { name: 'Refresh Results' })).toBeVisible();
+  //Item create in needed folder
   await page.locator('span').filter({ hasText: 'Charoite' }).getByLabel('Expand Folder').click();
   await page
     .locator('span')
     .filter({ hasText: 'Pavel`s items' })
     .getByLabel('Expand Folder')
     .click();
-
   const itemToRightClick = page.locator('.tree-item-title').filter({ hasText: 'For AUTO' });
   await itemToRightClick.click();
   await itemToRightClick.click({ button: 'right' });
   await page.locator('.tree-item-title').filter({ hasText: 'For AUTO' }).click({ button: 'right' });
-  await page.waitForTimeout(100);
+  await page.waitForTimeout(300);
   await page.keyboard.press('N');
   await page.waitForTimeout(1000);
   await page.getByRole('textbox', { name: 'Search' }).click();
@@ -44,8 +48,9 @@ test('Item create', async ({ page }) => {
     .locator('iframe[name="itemscope"]')
     .contentFrame()
     .locator('input[name="inplace_value"]')
-    .fill('General AUTO PA1');
+    .fill('General AUTO PA2');
   await page.keyboard.press('Enter');
+  //Change item status to Online and save the item
   const statusSelect = page
     .locator('iframe[name="itemscope"]')
     .contentFrame()
@@ -54,8 +59,7 @@ test('Item create', async ({ page }) => {
     .first();
   await statusSelect.click();
   await statusSelect.click();
-  await page.waitForTimeout(100);
-  //   const OnlineStatus = page.locator('.item-label').filter({ hasText: 'Online' });
+  await page.waitForTimeout(200);
   const OnlineStatus = page
     .locator('iframe[name="itemscope"]')
     .contentFrame()
@@ -64,6 +68,51 @@ test('Item create', async ({ page }) => {
     .nth(1);
   await OnlineStatus.click();
   await page.locator('#kms-action-bar-button-Save').filter({ visible: true }).dblclick();
+  await expect(
+    page.locator('iframe[name="itemscope"]').contentFrame().locator('#item-update-status-section'),
+  ).toContainText('Online');
+
+  //item create in needed folder and save it in Offline status
+  await page.getByRole('button', { name: 'Cancel' }).click();
+  // const itemToRightClick2 = page.locator('.tree-item-title').filter({ hasText: 'Charoite' });
+  // await itemToRightClick1.click();
+  // // await page.locator('span').filter({ hasText: 'Charoite' }).getByLabel('Expand Folder').click();
+  // await page
+  //   .locator('span')
+  //   .filter({ hasText: 'Pavel`s items' })
+  //   .getByLabel('Expand Folder')
+  //   .click();
+  const itemToRightClick1 = page.locator('.tree-item-title').filter({ hasText: 'For AUTO' });
+  await itemToRightClick1.click();
+  await itemToRightClick1.click({ button: 'right' });
+  await page.locator('.tree-item-title').filter({ hasText: 'For AUTO' }).click({ button: 'right' });
+  await page.waitForTimeout(300);
+  await page.keyboard.press('N');
+  await page.waitForTimeout(1000);
+  await page.getByRole('textbox', { name: 'Search' }).click();
+  await page.getByRole('textbox', { name: 'Search' }).fill('general');
+  await page
+    .getByRole('paragraph')
+    .filter({ hasText: /^General$/ })
+    .getByRole('img')
+    .click();
+  await page.getByRole('button', { name: 'Next' }).click();
+  await page.getByRole('button', { name: 'Skip' }).click();
+  await page
+    .locator('iframe[name="itemscope"]')
+    .contentFrame()
+    .getByRole('heading', { name: 'New Item' })
+    .click();
+  await page
+    .locator('iframe[name="itemscope"]')
+    .contentFrame()
+    .locator('input[name="inplace_value"]')
+    .fill('General AUTO PA3');
+  await page.keyboard.press('Enter');
+  await page.locator('#kms-action-bar-button-Save').filter({ visible: true }).dblclick();
+  await expect(
+    page.locator('iframe[name="itemscope"]').contentFrame().locator('#item-update-status-section'),
+  ).toContainText('Offline');
 });
 
 //   await expect(
