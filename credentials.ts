a87f97b9fe69c0a16244f2b841a35fbd8194f562
Aurumpasha <aurumpasha@gmail.com>
2025-04-27 17:53:31 +0800

    creds

---
+++ credentials.ts
@@ -0,0 +1,17 @@
+// credentials for CM user
+export const CREDENTIALS_CM = {
+  username: 'cm',
+  password: 'cm',
+};
+
+// credentials for CSR user
+export const CREDENTIALS_CSR = {
+  username: 'csr',
+  password: 'csr',
+};
+
+// credentials for ADMIN user
+export const CREDENTIALS_ADMIN = {
+  username: 'admin',
+  password: 'admin',
+};
